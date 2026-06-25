ef0bfc0105a23b35fffa8b04cb9ab49a72d2ba06
panktipatel2007 <patelpankti3201@gmail.com>
2026-06-25 13:24:30 +0530

    Update app run configuration to use environment port

--- ExpenseTracker/app.py
+++ ExpenseTracker/app.py
@@ -45,6 +45,9 @@ def delete_expense(id):
     db.session.commit()
     return redirect(url_for('index'))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
